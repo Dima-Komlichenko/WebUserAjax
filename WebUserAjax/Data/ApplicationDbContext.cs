c6f4db15710ec6f4cab144a36f7dec05a42ee349
Dima Komlichenko <aashziro@gmail.com>
2021-09-03 10:06:01 +0300

    Добавьте файлы проекта.

---
+++ WebUserAjax/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebUserAjax.Data.Entities;
+
+namespace WebUserAjax.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ProjectUser>
+    {
+
+        public DbSet<WebUserAjax.Data.Entities.School.Group> Groups { get; set; }
+        public DbSet<WebUserAjax.Data.Entities.School.Student> Students { get; set; }
+        public DbSet<WebUserAjax.Data.Entities.School.Teacher> Teachers { get; set; }
+
+        public DbSet<WebUserAjax.Data.Entities.Slider.SliderItem> SliderItem { get; set; }
+        public DbSet<WebUserAjax.Data.Entities.Slider.SliderGroup> SliderGroup { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
